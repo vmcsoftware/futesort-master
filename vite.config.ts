67057f22f9e40b958f31c1cc893be3b819442a2b
SEEMG <vinicius.machado.carvalho@educacao.mg.gov.br>
2025-12-03 14:14:47 -0300

    Resolver conflito de merge em vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -13,24 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
->>>>>>> b7498fe6c83f181618ed327d3aec4f63ecaa15c8
   server: {
     host: "::",
     port: 8080,
   },
-<<<<<<< HEAD
-  plugins: [react()],
-=======
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
->>>>>>> b7498fe6c83f181618ed327d3aec4f63ecaa15c8
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
   base: '/futesort-master/',
-})
-=======
 }));
->>>>>>> b7498fe6c83f181618ed327d3aec4f63ecaa15c8
